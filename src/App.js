4377b9458a69fca09158e919a094e748b2bb3907
scull1916 <kpscully@gmail.com>
2018-05-06 11:02:06 -0400

    application set-up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+var routes = require('./config/routes');
 
-export default App;
+ReactDOM.render(
+
+  <Router>{routes}</Router>,
+  document.getElementById('app')
+
+);
